2a6efe5afe0a81018ae13885b70421ceb037875c
Wicchi <74045262+Wicchi@users.noreply.github.com>
2023-12-20 15:54:48 +0100

    first

---
+++ app/download_preprocess.py
@@ -0,0 +1,42 @@
+import requests
+from pathlib import Path
+from gensim.models import KeyedVectors
+
+class DownloadPreprocess:
+    def __init__(self, url, output_file):
+        self.url = url
+        self.output_file = output_file
+
+    def download_word2vec_vectors(self):
+        try:
+            response = requests.get(self.url, stream=True)
+            response.raise_for_status()
+
+            with open(self.output_file, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
+        except requests.exceptions.RequestException as e:
+            print(f"Error during download: {e}")
+
+    def preprocess_word2vec_vectors(self):
+        try:
+            wv = KeyedVectors.load_word2vec_format(self.output_file, binary=True, limit=1000000)
+            wv.save_word2vec_format('vectors.csv')
+        except Exception as e:
+            print(f"Error during preprocessing: {e}")
+
+    def download_and_preprocess(self):
+        try:
+            if not Path(self.output_file).is_file():
+                print("Downloading Word2Vec vectors...")
+                self.download_word2vec_vectors()
+                print("Download complete.")
+
+                print("Preprocessing Word2Vec vectors...")
+                self.preprocess_word2vec_vectors()
+                print("Preprocessing complete.")
+            else:
+                print(f"File '{self.output_file}' already exists. Skipping download and preprocess.")
+        except Exception as e:
+            print(f"Error: {e}")
---
+++ app/main.py
@@ -0,0 +1,31 @@
+from download_preprocess import DownloadPreprocess
+from data_processing import DataProcessor
+import argparse
+
+# main.py
+
+def main():
+    parser = argparse.ArgumentParser(description='Process Word2Vec embeddings and calculate similarity.')
+    parser.add_argument('word_embeddings_file', type=str, help='Path to the Word2Vec embeddings file')
+    parser.add_argument('phrases_file', type=str, help='Path to the phrases file')
+    parser.add_argument('--mode', choices=['batch', 'on_the_fly'], default='on_the_fly', help='Execution mode: batch or on-the-fly (default: batch)')
+    args = parser.parse_args()
+
+    processor = DataProcessor(word_embeddings_file=args.word_embeddings_file, phrases_file=args.phrases_file)
+
+    if args.mode == 'batch':
+        similarity_matrix, top_n_names = processor.calculate_similarity_batch(top_n=5)
+        print(f"Similarity Matrix: {similarity_matrix}")
+        print("Top N Decoded Names:")
+        for i, names in enumerate(top_n_names):
+            print(f"Phrase {i + 1}: {', '.join(names)}")
+    elif args.mode == 'on_the_fly':
+        user_input = input("Enter a phrase: ")
+        closest_match, distance, top_n_names = processor.calculate_similarity_on_the_fly(user_input, top_n=5)
+        print(f"Closest Match: {closest_match}, Distance: {distance}")
+
+
+if __name__ == "__main__":
+    main()
+
+
